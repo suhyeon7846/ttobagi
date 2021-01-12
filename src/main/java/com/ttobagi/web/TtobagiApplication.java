64455bdb1d1aac0d65d8521f6e615aa61ba270a4
junjun-creator <kimbj910210@naver.com>
2021-01-12 18:17:25 +0900

    ddddd

---
+++ src/main/java/com/ttobagi/web/TtobagiApplication.java
@@ -0,0 +1,14 @@
+package com.ttobagi.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtobagiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtobagiApplication.class, args);
+		//initial
+	}
+
+}
---
+++ src/test/java/com/ttobagi/web/TtobagiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttobagi.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtobagiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
