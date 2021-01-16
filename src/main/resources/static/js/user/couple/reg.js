6d2b2aeaafe3e10eaa86a9ea8af83ec4a3a6243f
JaeHee Cho <40957996+cbw1030@users.noreply.github.com>
2021-01-16 17:32:35 +0900

    모달 디자인

--- src/main/resources/static/js/user/couple/reg.js
+++ src/main/resources/static/js/user/couple/reg.js
@@ -1,3 +1,22 @@
 window.addEventListener('load', (e) => {
+    const modalOpenBtn = document.querySelector('.modal-open');
+    const modalCloseBtn = document.querySelector('.modal-close');
+    const modalSecion = document.querySelector('.modal-sec');
 
+    const requestBtn = document.querySelector('.request-btn');
+    
+    modalOpenBtn.addEventListener('click', () => {
+        console.log('modal close');
+        modalSecion.style.display = 'flex';
+        //modalSecion.style.transition = '0.5s';
+    });
+
+    modalCloseBtn.addEventListener('click', () => {
+        console.log('modal open');
+        modalSecion.style.display = 'none';
+    });
+
+    requestBtn.addEventListener('click', () => {
+        alert('정상적으로 요청되었습니다.');
+    });
 });
