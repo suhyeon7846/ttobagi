e914530bfd40ca711b958c06ff4607386ab5242e
Sunflo-H <sunflo0524@gmail.com>
2021-01-25 18:31:38 +0900

    21/1/25

---
+++ src/main/java/com/ttobagi/web/controller/user/LetterController.java
@@ -0,0 +1,20 @@
+package com.ttobagi.web.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/letter")
+public class LetterController {
+	
+	@RequestMapping("list")
+	public String list() {
+		return "user.letter.list";
+	}
+	
+	@RequestMapping("reg")
+	public String reg() {
+		return "user.letter.reg";
+	}
+	
+}
